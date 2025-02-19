6c1ba0eb2bdb69c761d3d05f139298e38595b987
Thewa-AlienHub <117763807+Thewa-AlienHub@users.noreply.github.com>
2025-02-19 23:23:53 +0530

    "Initial Commit"

---
+++ src/test/java/org/example/pocketpilot/PocketPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pocketpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketPilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
